18c7052aa308fcf8ad94500b693af22ac3a6d762
Apoorva Pendse <102853901+apoorvapendse@users.noreply.github.com>
2023-10-30 18:56:49 +0530

    shifted everything to 2D

--- aprt-chess/src/App.tsx
+++ aprt-chess/src/App.tsx
@@ -2,12 +2,11 @@ import "./assets/App.css";
 import Board from "./components/Board";
 
 function App() {
-  const verticalBox = ["1", "2", "3", "4", "5", "6", "7", "8"];
-  const horizontalBox = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  
 
   return (
     <>
-      <Board verticalBox={verticalBox} horizontalBox={horizontalBox} />
+      <Board  />
     </>
   );
 }
