f2ae57d128b1908846ee66ad472027262eee5ef6
Rajeev Tapadia <rtapadia730@gmail.com>
2023-12-20 13:36:01 +0530

    created /dashboard and /board routes

--- aprt-chess/src/App.tsx
+++ aprt-chess/src/App.tsx
@@ -1,13 +1,16 @@
 import "./assets/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import Board from "./components/Board";
 
 function App() {
-  
-
   return (
-    <>
-      <Board  />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/board" element={<Board/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
