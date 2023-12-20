b3e48c05ec0947fddf933759397d41e3f64fbd7e
Rajeev Tapadia <rtapadia730@gmail.com>
2023-12-20 15:47:13 +0530

    basic peer to peer connection done

--- aprt-chess/src/components/Dashboard.tsx
+++ aprt-chess/src/components/Dashboard.tsx
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import Peer, { BufferedConnection, DataConnection } from "peerjs";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
-  let [peerID, setPeerID] = useState<string>("");
+  let [myPeer, setMyPeer] = useState<Peer>(new Peer());
+  let [myID, setMyID] = useState<string>("");
+  let [reciverID, setReciverID] = useState<string>("");
 
-  function createRoom() {}
-  function joinRoom() {}
+  useEffect(() => {
+    myPeer.on("open", function (id) {
+      console.log("My peer ID is: " + id);
+    });
+
+    myPeer.on("connection", (conn) => {
+      console.log("connection successful");
+      conn.on("data", (data) => {
+        console.log("recieved", data);
+      });
+    });
+  }, []);
+
+  function joinRoom() {
+    let connection: DataConnection = myPeer.connect(reciverID);
+    if (!connection) {
+      console.log("connection could not be established");
+      return;
+    }
+    connection.on("open", () => {
+      connection.send("helloooo");
+    });
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(myID);
+  }
 
   return (
     <div className="container mx-auto p-4 text-center">
@@ -13,26 +41,37 @@ const Dashboard = () => {
       <div className="mb-4">
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded mx-auto block"
-          onClick={createRoom}
+          onClick={() => setMyID(myPeer.id)}
         >
           Create a game
         </button>
       </div>
-
-      <div className="mb-4">
-        <div className="mb-2">Enter a code to join a game</div>
-        <input
-          className="border border-gray-300 px-4 py-2 rounded"
-          onChange={(e) => setPeerID(e.target.value)}
-          placeholder="code goes here"
-        />
-        <button
-          className="bg-green-500 text-white px-4 py-2 rounded ml-2"
-          onClick={joinRoom}
-        >
-          Join Game
-        </button>
-      </div>
+      {myID ? (
+        <div>
+          <div>{myID}</div>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded mx-auto block"
+            onClick={copyToClipboard}
+          >
+            Copy Code
+          </button>
+        </div>
+      ) : (
+        <div className="mb-4">
+          <div className="mb-2">Enter a code to join a game</div>
+          <input
+            className="border border-gray-300 px-4 py-2 rounded"
+            onChange={(e) => setReciverID(e.target.value)}
+            placeholder="code goes here"
+          />
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded ml-2"
+            onClick={joinRoom}
+          >
+            Join Game
+          </button>
+        </div>
+      )}
     </div>
   );
 };
