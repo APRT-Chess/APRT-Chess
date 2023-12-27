01a9fdc6767f2d20bd6ab0098269f4b91a34df1e
Apoorva Pendse <apoorvavpendse@gmail.com>
2023-12-27 16:33:30 +0530

    fix : castling now works for both colors

--- aprt-chess/src/utils/pathChecks/castleValidation.ts
+++ aprt-chess/src/utils/pathChecks/castleValidation.ts
@@ -1,6 +1,6 @@
 import { useBoard } from "../../contexts/BoardContext";
 import { Piece } from "../../types/global";
-import { whiteRook } from "../ChessPieces";
+import { blackRook, whiteRook } from "../ChessPieces";
 
 
 export function validateCastle(
@@ -16,22 +16,25 @@ export function validateCastle(
     if(fromX==4 && toX==6 && fromY==7 && toY == 7 ){
         console.log("castling attempted on the right");
         console.log(boardState[7][5],boardState[7][6]);
-        if(boardState[7][5]==""&&boardState[7][6] =="" && boardState[7][7] ==whiteRook){
+        if(boardState[7][5]==""&&boardState[7][6] =="" && boardState[7][7] ==whiteRook||boardState[7][7] ==blackRook){
+            console.log("valid castle move")
             const existingBoardState:Piece[][] = [...boardState];
-            existingBoardState[7][5] = whiteRook;
+            existingBoardState[7][5] = boardState[7][7]==blackRook? blackRook:whiteRook;
             existingBoardState[7][7] = "";
-       
+            
             //TODO: Find a way to setBoardState(existingBoardState) at this point
             
             return true;
         }
     }
-    else if(fromX==4 && toX==1 && fromY==7 && toY == 7 ){
+    else if(fromX==4 && toX==2 && fromY==7 && toY == 7 ){
         console.log("castling attempted on the left");
-       
-        if(boardState[7][3]==""&&boardState[7][2] =="" &&boardState[7][1] ==""&& boardState[7][0] ==whiteRook){
+        
+        if(boardState[7][3]==""&&boardState[7][2] =="" &&boardState[7][1] ==""&& boardState[7][0] ==whiteRook||boardState[7][0] ==blackRook){
+            console.log("valid castle move")
+            
             const existingBoardState:Piece[][] = [...boardState];
-            existingBoardState[7][2] = whiteRook;
+            existingBoardState[7][3] = boardState[7][0]== blackRook? blackRook:whiteRook;
             existingBoardState[7][0] = "";
             
             //TODO: Find a way to setBoardState(existingBoardState) at this point
