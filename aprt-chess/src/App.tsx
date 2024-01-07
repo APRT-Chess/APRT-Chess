98889dedb1a2e42bfefa3b5b5289b5f2a36e0bc8
Rajeev Tapadia <rtapadia730@gmail.com>
2024-01-07 14:26:30 +0530

    add: /login route

--- aprt-chess/src/App.tsx
+++ aprt-chess/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Board from "./components/Board";
+import Login from "./components/Login";
 import { Peer } from "peerjs";
 import { PieceColor } from "./types/global";
 import { BoardProvider } from "./contexts/BoardContext";
@@ -41,6 +42,9 @@ function App() {
               />
             }
           ></Route>
+
+          <Route path="/login" element={<Login />} />
+          
           <Route
             path="/board"
             element={
