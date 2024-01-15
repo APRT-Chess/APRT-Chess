3874e70b4733b638b8c758e100bd78ebf93232d7
Rajeev Tapadia <rtapadia730@gmail.com>
2024-01-15 22:33:10 +0530

    added comments in castleValidation

--- aprt-chess/src/utils/pathChecks/castleValidation.ts
+++ aprt-chess/src/utils/pathChecks/castleValidation.ts
@@ -2,6 +2,7 @@ import { Piece } from "../../types/global";
 import { blackRook, whiteRook } from "../ChessPieces";
 import { socket } from "../socket/socket";
 
+// this function is triggered for any movement of king
 export function validateCastle(
   fromX: number,
   fromY: number,
@@ -9,9 +10,9 @@ export function validateCastle(
   toY: number,
   boardState: Piece[][]
 ): boolean {
+  // following block to check if casteling is attempted to right
   if (fromX == 4 && toX == 6 && fromY == 7 && toY == 7) {
-    console.log("castling attempted on the right");
-    console.log(boardState[7][5], boardState[7][6]);
+    // todo: fix this if case & make it reversible for both colors
     if (
       (boardState[7][5] == "" &&
         boardState[7][6] == "" &&
@@ -22,13 +23,13 @@ export function validateCastle(
       existingBoardState[7][5] =
         boardState[7][7] == blackRook ? blackRook : whiteRook;
       existingBoardState[7][7] = "";
-    socket.emit("update-board",existingBoardState)
+      socket.emit("update-board", existingBoardState);
 
       return true;
     }
-  } else if (fromX == 4 && toX == 2 && fromY == 7 && toY == 7) {
-    console.log("castling attempted on the left");
-
+  }
+  // following block to check if casteling is attempted to left
+  else if (fromX == 4 && toX == 2 && fromY == 7 && toY == 7) {
     if (
       (boardState[7][3] == "" &&
         boardState[7][2] == "" &&
@@ -40,7 +41,7 @@ export function validateCastle(
       existingBoardState[7][3] =
         boardState[7][0] == blackRook ? blackRook : whiteRook;
       existingBoardState[7][0] = "";
-    socket.emit("update-board",existingBoardState)
+      socket.emit("update-board", existingBoardState);
       return true;
     }
   }
--- aprt-chess/src/utils/pieceValidation/king.ts
+++ aprt-chess/src/utils/pieceValidation/king.ts
@@ -1,7 +1,6 @@
-
 import { Piece } from "../../types/global";
 import { calcDist } from "../mathFunctions";
-import {validateCastle} from "../pathChecks/castleValidation";
+import { validateCastle } from "../pathChecks/castleValidation";
 
 // king move is valid iff Math.floor(dist(to, from)) === 1
 // kill move not yet implemented
@@ -11,18 +10,15 @@ export function validateKingMove(
   fromY: number,
   toX: number,
   toY: number,
-  boardState:Piece[][]
- 
+  boardState: Piece[][]
 ): boolean {
-
   //checking castling conditions
 
-  const isValidCastle = validateCastle(fromX,fromY,toX,toY,boardState);
-  if(isValidCastle)return true;
+  const isValidCastle = validateCastle(fromX, fromY, toX, toY, boardState);
+  if (isValidCastle) return true;
 
   const distance = Math.floor(calcDist(fromX, fromY, toX, toY));
   console.log(distance);
-  if (distance === 1) 
-    return true;
+  if (distance === 1) return true;
   else return false;
 }
