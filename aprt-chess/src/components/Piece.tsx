cac7faa2b1bacdf90852802cab8fe1407b9be80f
Rajeev Tapadia <rtapadia730@gmail.com>
2024-02-04 14:56:52 +0530

    feat: highlighing pieces that can be captured

--- aprt-chess/src/components/Piece.tsx
+++ aprt-chess/src/components/Piece.tsx
@@ -4,9 +4,10 @@ interface props {
   image: string;
   x_coordinate: number;
   y_coordinate: number;
+  inDanger: boolean;
 }
 
-function Piece({ image, x_coordinate, y_coordinate }: props) {
+function Piece({ image, x_coordinate, y_coordinate, inDanger }: props) {
   function handleDrag(e: DragEvent<HTMLImageElement>) {
     // extracting the piece name(eg wK) from image url
     const pathString = image.split("/");
@@ -24,7 +25,9 @@ function Piece({ image, x_coordinate, y_coordinate }: props) {
     <img
       src={image}
       alt="piece-img"
-      className={`x-${x_coordinate} y-${y_coordinate} w-24 h-24 absolute`}
+      className={`x-${x_coordinate} y-${y_coordinate} w-24 h-24 absolute ${
+        inDanger ? "border-4 border-red-500" : ""
+      }`}
       draggable
       onDragStart={(e) => handleDrag(e)}
     />
