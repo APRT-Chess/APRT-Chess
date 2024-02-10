948dc95444a7ca2657820f18a3e701abdda8dc81
Rajeev Tapadia <rtapadia730@gmail.com>
2024-02-10 21:53:15 +0530

    connected to onrender backend

--- aprt-chess/src/utils/socket/socket.ts
+++ aprt-chess/src/utils/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:4000';
+const URL = 'https://chess-backend-foom.onrender.com';
 
 export const socket = io(URL);
