4abb9e0b8a6d511afa4fbc60e1a5c5e6d0928ebc
Rajeev Tapadia <rtapadia730@gmail.com>
2024-02-16 18:19:49 +0530

    test: updated knight unit tests

--- aprt-chess/src/tests/pieceValidation/knight.test.ts
+++ aprt-chess/src/tests/pieceValidation/knight.test.ts
@@ -1,16 +1,29 @@
-import { test, expect } from 'vitest';
-import { validateKnightMove } from '../../utils/pieceValidation/knight';
+import { test, expect } from "vitest";
+import { validateKnightMove } from "../../utils/pieceValidation/knight";
 
-test('case: valid knight move right down', async () => {
+const emptyBoard: string[][] = [
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+];
 
-  const isValid = validateKnightMove(1, 2, 3, 1);
+test("case: valid knight move right down", async () => {
+  const board = structuredClone(emptyBoard);
+  board[2][1] = "/public/pieces/bK.svg";
+  const isValid = validateKnightMove(1, 2, 3, 1, board);
 
   expect(isValid).toBe(true);
 });
 
-test('case: invalid knight move', async () => {
-
-  const isValid = validateKnightMove(1, 2, 4, 5);
+test("case: invalid knight move", async () => {
+  const board = structuredClone(emptyBoard);
+  board[2][1] = "/public/pieces/bK.svg";
+  const isValid = validateKnightMove(1, 2, 4, 5, board);
 
   expect(isValid).toBe(false);
 });
