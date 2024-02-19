4d271226c31fab02572ec5725040d9017db0837a
Rajeev Tapadia <rtapadia730@gmail.com>
2024-02-19 21:55:19 +0530

    fix: king and knight friendly fire

--- aprt-chess/src/utils/pieceValidation/knight.ts
+++ aprt-chess/src/utils/pieceValidation/knight.ts
@@ -1,15 +1,18 @@
+import { Piece } from "../../types/global";
 import { calcDist, calcSlope } from "../mathFunctions";
+import { validateCapture } from "../pathChecks/validateCaptureForOppositeColor";
 
 export function validateKnightMove(
   fromX: number,
   fromY: number,
   toX: number,
   toY: number,
+  boardState: Piece[][]
 ): boolean {
   const slope = Math.abs(calcSlope(fromX, fromY, toX, toY));
   const distance = Math.floor(calcDist(fromX, fromY, toX, toY));
 
   if ((slope === 0.5 || slope === 2) && distance === 2) {
-    return true;
+    return validateCapture(fromX, fromY, toX, toY, boardState);
   } else return false;
 }
